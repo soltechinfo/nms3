7428accd7f1d9d3ca5f84cbcaba6a5663ae4d62c
soltechinfo <soltech@soltech.co.kr>
2023-11-29 14:04:57 +0900

    FOOT CHANGE

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ import "./footer.scss"
 const Footer = () => {
   return (
     <div className="footer">
-      <span>lamadmin</span>
-      <span>© Lama Dev Admin Dashboard</span>
+      <span>SOLTECH</span>
+      <span>© ALL RIGHT RESERVED</span>
     </div>
   )
 }
